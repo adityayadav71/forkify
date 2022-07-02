6f9173e1bb05500ad67bcda8a21c7d9c0da49681
adityayadav71 <adityayadav1088@gmail.com>
2022-07-02 07:48:09 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = 'key=cefdd12f-e4ec-4f7b-b4cc-0a5a5b5a612a';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
